239b1502d9ab29deb82d19e936e8c30994bcc403
ceev-roy <vishalsyadav2005@gmail.com>
2024-09-24 19:48:23 +0530

    Add files via upload

---
+++ src/components/Weather.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Weather.css";
+import searchimg from "../Assets/search.png";
+import clear from "../Assets/clear.png";
+import cloud from "../Assets/cloud.png";
+import drizzle from "../Assets/drizzle.png";
+import humidity from "../Assets/humidity.png";
+import rain from "../Assets/rain.png";
+import snow from "../Assets/snow.png";
+import wind from "../Assets/wind.png";
+
+const Weather = () => {
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear,
+    "01n": clear,
+    "02d": cloud,
+    "02n": cloud,
+    "03d": cloud,
+    "03n": cloud,
+    "04d": drizzle,
+    "04n": drizzle,
+    "09d": rain,
+    "09n": rain,
+    "10d": rain,
+    "10n": rain,
+    "13d": snow,
+    "13n": snow,
+  };
+
+  const search = async (city) => {
+    if (city === "") {
+      alert("Enter City Name");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+      // console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      setWeatherData(false);
+      console.error("Error in fetching weather data");
+    }
+    inputRef.current.value = "";
+  };
+
+  useEffect(() => {
+    search("New York");
+  }, []);
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      // alert("enter click");
+      search(inputRef.current.value);
+    }
+  };
+  // handleKeyPress = (e) => {
+  //   if (e.key === "Enter") {
+  //     search(inputRef.current.value);
+  //   }
+  // };
+
+  return (
+    <div className="weather">
+      <div className="search-box">
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Search"
+          onKeyDown={handleKeyDown}
+        />
+        <img
+          src={searchimg}
+          alt=""
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+      {weatherData ? (
+        <>
+          <img src={weatherData.icon} alt="" className="weather-icon" />
+          <p className="temperature">{weatherData.temperature}°c</p>
+          <p className="location">{weatherData.location}</p>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity} alt="" />
+              <div>
+                <p>{weatherData.humidity} %</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind} alt="" />
+              <div>
+                <p>{weatherData.windSpeed} Km/h</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
